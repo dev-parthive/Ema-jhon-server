138e4cb62412a0ead4a115c3dc7276b0a3ff3a86
dev-parthive <dev-parthive>
2022-11-13 20:57:01 +0600

    pagination done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =  require("express")
 const cors = require('cors')
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000 ;
 
@@ -22,16 +22,26 @@ async function run(){
      
 
         app.get('/products', async(req,res )=>{
-            const page = req.query.page
-            const size = req.query.size;
-            console.log(page , size)
+            const page = parseInt(req.query.page)
+            const size = parseInt(req.query.size);
+            // console.log(page , size)
             const query = {}
             const cursor = productCollection.find(query)
             // const products = await cursor.limit(10).toArray() 
-            const products = await cursor.toArray() 
+            const products = await cursor.skip(page*size).limit(size).toArray() 
             const count = await productCollection.estimatedDocumentCount()
 
             res.send({count, products})
+        });
+
+        app.post('/productsByIds', async (req, res) => {
+            const ids = req.body;
+            console.log(ids)
+            const objectIds = ids.map(id => ObjectId(id))
+            const query = { _id: { $in: objectIds } };
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
         })
 
     }
